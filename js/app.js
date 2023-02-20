151b5b7ee81b2ad71d89d66e73e599d99941f47a
Andy Woodward <andy.woodward1967@gmail.com>
2023-02-20 13:55:28 +0000

    Create the ability to add a new task

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 const taskContainer = document.querySelector('.task-container')
+const submitButton = document.querySelector('.submit-button')
 
 let tasks = [
   {
     name: 'Practice CSS Animations',
-    priority: 1,
+    priority: 0,
   },
   {
     name: 'Dev Community work',
-    priority: 4,
+    priority: 2,
   },
   {
     name: 'Algorithm Studies',
-    priority: 3,
+    priority: 1,
   },
 ]
 
@@ -48,3 +49,19 @@ function deleteTask(e) {
   e.target.parentNode.remove()
   // TODO Remove task from array after deleting
 }
+
+function addTask() {
+  const inputElement = document.querySelector('input')
+  const value = inputElement.value
+  console.log(value)
+  if (value) {
+    taskContainer.innerHTML = ''
+    tasks.push({
+      name: value,
+      priority: tasks.length,
+    })
+    render()
+  }
+}
+
+submitButton.addEventListener('click', addTask)
